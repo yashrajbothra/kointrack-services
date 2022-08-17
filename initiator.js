a8dfbe221d7ac2932ef9584bb7e883fc82e1d112
Bellamkonda Sivaji <sivaji.b@kointrack.com>
2022-08-17 18:18:54 +0530

    deleted batches file and added  jobs file and refactored that file into clean code

--- initiator.js
+++ initiator.js
@@ -1,10 +1,9 @@
 const logger = require('./utils/logger');
-const batches = require('./batches');
+const batches = require('./constants/jobs');
 const service = require('./services');
 
 const args = process.argv.slice(2);
 const currBatchName = args.pop();
-
 const startService = async (currJob) => {
   logger.warn(`${currJob.name} is started \n`);
   await service[currJob.serviceName](currJob.url, await currJob.getParams());
