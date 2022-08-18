521ff9a0c55674753656c013f52d2b33327e14a1
Bellamkonda Sivaji <sivaji.b@kointrack.com>
2022-08-18 13:43:55 +0530

    added batches in correct format

--- batches.js
+++ batches.js
@@ -14,9 +14,22 @@ const jobs = require('./constants/jobs');
  */
 
 module.exports = {
-  'batch-1': [jobs.GLOBAL_METRICS_LATEST, jobs.CRYPTOCURRENCY_MAP],
-  'batch-2': [jobs.CRYPTOCURRENCY_METADATA],
-  'batch-3': [jobs.CRYPTOCURRENCY_MARKET_DETAILS, jobs.OHLCV],
-  'batch-4': [jobs.BINANCE],
-  'batch-5': [jobs.CRYPTOCURRENCY_TRENDING_METRICS, jobs.CRYPTOCURRENCY_MOST_VISITED_METRICS],
+  'batch-1': [
+    jobs.GLOBAL_METRICS_LATEST,
+    jobs.CRYPTOCURRENCY_MAP,
+  ],
+  'batch-2': [
+    jobs.CRYPTOCURRENCY_METADATA,
+  ],
+  'batch-3': [
+    jobs.CRYPTOCURRENCY_MARKET_DETAILS,
+    jobs.OHLCV,
+  ],
+  'batch-4': [
+    jobs.BINANCE,
+  ],
+  'batch-5': [
+    jobs.CRYPTOCURRENCY_TRENDING_METRICS,
+    jobs.CRYPTOCURRENCY_MOST_VISITED_METRICS,
+  ],
 };
