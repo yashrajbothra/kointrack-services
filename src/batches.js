4a69aa2075fa831b81c76f453564d0bf8535412b
yashbothra-kointrack <yash.bothra@kointrack.com>
2022-08-22 11:36:49 +0530

    Logger Issues Fixed

--- src/batches.js
+++ src/batches.js
@@ -10,10 +10,10 @@ const jobs = require('./constants/jobs');
 module.exports = {
   'batch-1': [jobs.CRYPTOCURRENCY_MAP],
   'batch-2': [
-    jobs.CRYPTOCURRENCY_METADATA,
     jobs.CRYPTOCURRENCY_TRENDING_METRICS,
     jobs.CRYPTOCURRENCY_MOST_VISITED_METRICS,
     jobs.CRYPTOCURRENCY_MARKET_DETAILS,
+    jobs.CRYPTOCURRENCY_METADATA,
   ],
   'batch-3': [jobs.OHLCV, jobs.GLOBAL_METRICS_LATEST],
 };
--- src/utils/logger.js
+++ src/utils/logger.js
@@ -8,32 +8,90 @@ const enumerateErrorFormat = winston.format((info) => {
   return info;
 });
 
-const logger = winston.createLogger({
-  level: 'info',
-  format: winston.format.combine(
-    enumerateErrorFormat(),
-    winston.format.splat(),
-    winston.format.timestamp(),
-    winston.format.printf(
-      (info) => `${info.timestamp} ${info.level}: ${info.message}`,
+let logger;
+
+if (process.env.NODE_ENV === 'production') {
+  logger = winston.createLogger({
+    level: 'error',
+    format: winston.format.combine(
+      enumerateErrorFormat(),
+      winston.format.splat(),
+      winston.format.label(),
+      winston.format.timestamp(),
+      winston.format.printf(
+        (info) => `${info.timestamp} ${info.level}: ${info.label} :${info.message}`,
+      ),
     ),
-  ),
-  transports: [
-    new winston.transports.Console({
-      stderrLevels: ['error'],
-      format: winston.format.colorize({ all: true }),
-    }),
-    new WinstonDailyRotateFile({
-      filename: './logs/error/%DATE%.log',
-      datePattern: 'DD-MM-YYYY',
-      level: 'error',
-    }),
-    new WinstonDailyRotateFile({
-      filename: './logs/info/%DATE%.log',
-      datePattern: 'DD-MM-YYYY',
-      level: 'info',
-    }),
-  ],
-});
+    transports: [
+      new winston.transports.Console({
+        stderrLevels: ['error'],
+        format: winston.format.colorize({ all: true }),
+      }),
+      new WinstonDailyRotateFile({
+        filename: './logs/prod/error/%DATE%.log',
+        datePattern: 'DD-MM-YYYY',
+        level: 'error',
+      }),
+    ],
+  });
+} else if (process.env.NODE_ENV === 'stage') {
+  logger = winston.createLogger({
+    level: 'warn',
+    format: winston.format.combine(
+      enumerateErrorFormat(),
+      winston.format.splat(),
+      winston.format.label(),
+      winston.format.timestamp(),
+      winston.format.printf(
+        (info) => `${info.timestamp} ${info.level}: ${info.label} :${info.message}`,
+      ),
+    ),
+    transports: [
+      new winston.transports.Console({
+        stderrLevels: ['error'],
+        format: winston.format.colorize({ all: true }),
+      }),
+      new WinstonDailyRotateFile({
+        filename: './logs/stage/error/%DATE%.log',
+        datePattern: 'DD-MM-YYYY',
+        level: 'error',
+      }),
+      new WinstonDailyRotateFile({
+        filename: '.logs/stage/warn/%DATE%.log',
+        datePattern: 'DD-MM-YYYY',
+        level: 'warn',
+      }),
+    ],
+  });
+} else {
+  logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.combine(
+      enumerateErrorFormat(),
+      winston.format.splat(),
+      winston.format.label(),
+      winston.format.timestamp(),
+      winston.format.printf(
+        (info) => `${info.timestamp} ${info.level}: ${info.label} :${info.message}`,
+      ),
+    ),
+    transports: [
+      new winston.transports.Console({
+        stderrLevels: ['error'],
+        format: winston.format.colorize({ all: true }),
+      }),
+      new WinstonDailyRotateFile({
+        filename: './logs/dev/error/%DATE%.log',
+        datePattern: 'DD-MM-YYYY',
+        level: 'error',
+      }),
+      new WinstonDailyRotateFile({
+        filename: './logs/dev/info/%DATE%.log',
+        datePattern: 'DD-MM-YYYY',
+        level: 'info',
+      }),
+    ],
+  });
+}
 
 module.exports = logger;
